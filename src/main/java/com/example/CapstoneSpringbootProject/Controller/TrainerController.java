540f04e17465a8c3112e92c344ae13ef44f56620
nathanmurfy <nathanmurphyy02@gmail.com>
2021-08-17 10:07:17 +1000

    Added bootstrap and changed controller

--- src/main/java/com/example/CapstoneSpringbootProject/Controller/TrainerController.java
+++ src/main/java/com/example/CapstoneSpringbootProject/Controller/TrainerController.java
@@ -5,19 +5,19 @@ import com.example.CapstoneSpringbootProject.Entity.Trainer;
 import com.example.CapstoneSpringbootProject.Service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("api/trainer")
 public class TrainerController {
 
-    @Autowired
-    private TrainerService trainerService;
-
-    @PostMapping("/add")
-    public void insertTrainer(Trainer trainer){
-        trainerService.insertTrainer(trainer);
+    @GetMapping
+    String getTrainer(Model model){
+        model.addAttribute("something", "this is from the controller");
+        return "trainer";
     }
+
 }
